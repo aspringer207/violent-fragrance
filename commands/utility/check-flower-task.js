337b9a6d4d6bc2feeea777d3fd5d494402acdc71
Andrea Springer <69414777+aspringer207@users.noreply.github.com>
2026-04-07 09:47:56 -0400

    fix dc commands for check flower and report flower

---
+++ commands/utility/check-flower-task.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("discord.js");
+const sql = require("../../db");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("check-flower-task")
+    .setDescription(
+      "Search a specific flower to see how many members can complete it.",
+    )
+    .addStringOption((option) =>
+      option
+        .setName("flower")
+        .setDescription("The name of the flower to search for")
+        .setRequired(true),
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+    const flowerName = interaction.options.getString("flower");
+    const result = await sql`
+select
+  count(*)
+from
+  tcf.member_flowers
+  join tcf.flowers on tcf.flowers.flower_id = tcf.member_flowers.flower_id
+  join tcf.questing_status on tcf.member_flowers.member_id = tcf.questing_status.tcf_id
+where
+  tcf.questing_status.actively_questing
+  and (tcf.flowers.flower_name = ${flowerName})
+    `;
+    const count = result[0].count;
+    await interaction.editReply(
+      `There are ${count} members who can complete the ${flowerName} task.`,
+    );
+  },
+};
--- commands/utility/report-flower.js
+++ commands/utility/report-flower.js
@@ -1,9 +1,8 @@
 const { SlashCommandBuilder } = require("discord.js");
-const flowerList = require("../../flowerList");
-const flowerMap = require("../../flowerMap");
 const memberMap = require("../../memberMap");
 const createFlowerEntry = require("../../functions/createFlowerEntry");
-const createProcessString = require("../../functions/createProcessString")
+const createProcessString = require("../../functions/createProcessString");
+const getFlowerList = require("../../functions/getFlowerList");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,45 +20,52 @@ module.exports = {
     await interaction.deferReply();
     try {
       const flowerName = interaction.options.getString("flower");
-      const memberID = interaction.user.id
-      const tcfID = memberMap.get(memberID)
+      const memberID = interaction.user.id;
+      const tcfID = memberMap.get(memberID);
       let flowerID;
-      if (flowerMap.has(flowerName)){
-        flowerID = flowerMap.get(flowerName)
-      }
-      else if(flowerList.map((x)=> createProcessString(x[0])).includes(createProcessString(flowerName))){ 
-        const flowerNameProcessStrings = flowerList.map((x)=> createProcessString(x[0]));
-        const idx = flowerNameProcessStrings.findIndex((val) => val === createProcessString(flowerName))
-        flowerID = flowerList[idx][1];
-        await createFlowerEntry(tcfID, flowerID)
 
+      const flowerList = await getFlowerList().then((result) => result.map((x) => [x.flower_name, x.flower_id]));
+      const flowerMap = await getFlowerList().then((result) => new Map(result.map((x) => [x.flower_name, x.flower_id])));
+
+      if (flowerMap.has(flowerName)) {
+        flowerID = flowerMap.get(flowerName);
+      } else if (
+        flowerList
+          .map((x) => createProcessString(x[0]))
+          .includes(createProcessString(flowerName))
+      ) {
+        const flowerNameProcessStrings = flowerList.map((x) =>
+          createProcessString(x[0]),
+        );
+        const idx = flowerNameProcessStrings.findIndex(
+          (val) => val === createProcessString(flowerName),
+        );
+        flowerID = flowerList[idx][1];
+        await createFlowerEntry(tcfID, flowerID);
       } else {
-        throw new ReferenceError("Flower not found in database.")
+        throw new ReferenceError("Flower not found in database.");
       }
       await createFlowerEntry(tcfID, flowerID);
 
-      
-      const response = `How luxurious! You can grow ${flowerName}! Thank you for the update, ${interaction.user.tag}!`
+      const response = `How luxurious! You can grow ${flowerName}! Thank you for the update, ${interaction.user.tag}!`;
       await interaction.editReply(response);
-
-
     } catch (error) {
-      if (error instanceof ReferenceError){
+      if (error instanceof ReferenceError) {
         console.error("Reference ERROR:", error);
-       await interaction.editReply(
-        `I don't recognize that flower, sorry. Try typing the name again, or run the /flower-list command to find the name in the list.`,
-      );
-      } else if (error.code === "23505"){
+        await interaction.editReply(
+          `I don't recognize that flower, sorry. Try typing the name again, or run the /flower-list command to find the name in the list.`,
+        );
+      } else if (error.code === "23505") {
         await interaction.editReply(
           `Silly goose! You already have that flower. Oh well, better a silly goose than a crazy duck!`,
         );
-      } else
-      {
+      } else {
         console.error("SQL ERROR:", error);
-      console.error("Node ERROR:", error);
-      await interaction.editReply(
-        `This command is still in development, but Andrea is working hard to get it up and running! Please be patient and don't yell at her. I am a nice bot.`,
-      );}
+        console.error("Node ERROR:", error);
+        await interaction.editReply(
+          `This command is still in development, but Andrea is working hard to get it up and running! Please be patient and don't yell at her. I am a nice bot.`,
+        );
+      }
     }
   },
 };
