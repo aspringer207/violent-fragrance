f6c70cee665d43ed491c70cce050ef7da6f6985d
Andrea Springer <69414777+aspringer207@users.noreply.github.com>
2026-04-08 19:56:39 -0400

    fix error handling in report-flower

--- commands/utility/report-flower.js
+++ commands/utility/report-flower.js
@@ -21,6 +21,9 @@ module.exports = {
     try {
       const flowerName = interaction.options.getString("flower");
       const memberID = interaction.user.id;
+      const userNickname = interaction.member.nick
+        ? interaction.member.nick
+        : interaction.user.username;
       const tcfID = memberMap.get(memberID);
       let flowerID;
 
@@ -49,27 +52,57 @@ module.exports = {
       } else {
         throw new ReferenceError("Flower not found in database.");
       }
-      await createFlowerEntry(tcfID, flowerID);
-
-      const response = `How luxurious! You can grow ${flowerName}! Thank you for the update, ${interaction.user.tag}!`;
-      await interaction.editReply(response);
+      await createFlowerEntry(tcfID, flowerID)
+        .then((response) =>
+          interaction.editReply(
+            `So prestigious! You can grow ${response.flower_name}! Thank you for the update, ${userNickname}!`,
+          ),
+        )
+        .catch(async (error) => {
+          if (error.code === "23505") {
+            const duplicateFlowerLog = {
+              userName: userNickname,
+              userInput: flowerName,
+              errorName: error.name,
+              errorCode: error.code,
+              errorMessage: error.message,
+            };
+            console.table(duplicateFlowerLog);
+            await interaction.editReply(
+              `Silly goose! You already have that flower. Oh well, better a silly goose than a creepy duck!`,
+            );
+            return;
+          }
+          if (error instanceof ReferenceError) {
+            const unrecognizedFlowerLog = {
+              userName: userNickname,
+              userInput: flowerName,
+              errorName: error.name,
+              errorCode: error.code,
+              errorMessage: error.message,
+            };
+            console.table(unrecognizedFlowerLog);
+            await interaction.editReply(
+              `I don't recognize that flower, sorry. Try typing the name again, or run the /list-flowers command to find the name in the list.`,
+            );
+            return;
+          }
+          const unknownErrorLog = {
+            userName: userNickname,
+            userInput: flowerName,
+            errorName: error.name,
+            errorCode: error.code,
+            errorMessage: error.message,
+          };
+          console.table(unknownErrorLog);
+          await interaction.editReply('Command failed due to an unknown error.');
+        });
     } catch (error) {
-      if (error instanceof ReferenceError) {
-        console.error("Reference ERROR:", error);
-        await interaction.editReply(
-          `I don't recognize that flower, sorry. Try typing the name again, or run the /flower-list command to find the name in the list.`,
-        );
-      } else if (error.code === "23505") {
-        await interaction.editReply(
-          `Silly goose! You already have that flower. Oh well, better a silly goose than a crazy duck!`,
-        );
-      } else {
-        console.error("SQL ERROR:", error);
-        console.error("Node ERROR:", error);
-        await interaction.editReply(
-          `This command is still in development, but Andrea is working hard to get it up and running! Please be patient and don't yell at her. I am a nice bot.`,
-        );
-      }
+      console.error("SQL ERROR:", error);
+      console.error("Node ERROR:", error);
+      await interaction.followUp(
+        `This command is still in development, but Andrea is working hard to get it up and running! Please be patient and don't yell at her. I am a nice bot.`,
+      );
     }
   },
 };
